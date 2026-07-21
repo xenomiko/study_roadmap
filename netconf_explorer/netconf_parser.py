35130c4d8f12eda56a1210b6cb1245f2941c85d3
GREENboy0549 <mehdasohaib@gmail.com>
2026-07-21 15:01:41 +0100

    feat: add Postman collections and sanitized environment config

--- netconf_explorer/netconf_parser.py
+++ netconf_explorer/netconf_parser.py
@@ -1,11 +1,13 @@
-import xml.etree.ElementTree as ET
 import yaml
-with open("devices.yaml", "r") as f:
+import xmltodict
+import json
+with open('devices.yaml','r') as f:
     devices = yaml.safe_load(f)
 
 for device in devices["devices"]:
-    netconf_args = device['netconf_args']
-    with open(f"config_{netconf_args["host"]}.xml", "r") as f:
-        xml_data = f.read()
-
-    print(xml_data)
+    netconf_args = device["netconf_args"]
+    filename = f"config_{netconf_args["host"]}.xml"
+    with open(filename, 'r') as file:
+        xml_data = file.read()
+    parsed_data = xmltodict.parse(xml_data)
+    print(type(parsed_data['rpc-reply']['data']['interfaces']['interface']))
